c3896bc0fa56cdf5e623f6242ca8df3a20b39e17
nicola00 <nicolajobs00@gmail.com>
2023-02-14 14:35:05 +0100

    START

---
+++ app.js
@@ -0,0 +1,19 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const membriTeam = {
+    Nome: 'Wayne Barnett',
+    Ruolo: 'Founder & CEO',
+    Foto: 'wayne-barnett-founder-ceo.jpg'
+}
+
+console.log(membriTeam)
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto (in questo fase usiamo il ciclo for…in )
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sotto forma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede (trovato un esempio allegato, ma siete liberi di gestire la grafica a vostro piacimento)
