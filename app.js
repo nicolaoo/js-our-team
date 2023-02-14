68abe0f3cb6a3f3274be03d9266c18fe007e66d7
nicola00 <nicolajobs00@gmail.com>
2023-02-14 14:42:34 +0100

    FEAT create array of object

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
-const membriTeam = {
+const membriTeam =[ {
     Nome: 'Wayne Barnett',
     Ruolo: 'Founder & CEO',
     Foto: 'wayne-barnett-founder-ceo.jpg'
-}
+},
+{
+    Nome: 'Angela Caroll',
+    Ruolo: 'Chief Editor',
+    Foto: 'angela-caroll-chief-editor.jpg'
+},
+{
+    Nome: 'Walter Gordon',
+    Ruolo: 'Office Manager',
+    Foto: 'walter-gordon-office-manager.jpg'
+},
+{
+    Nome: 'Angela Lopez',
+    Ruolo: 'Social Media Manager',
+    Foto: 'angela-lopez-social-media-manager.jpg'
+},
+{
+    Nome: 'Scott Estrada',
+    Ruolo: 'Developer',
+    Foto: 'scott-estrada-developer.jpg'
+},
+{
+    Nome: 'Barbara Ramos',
+    Ruolo: 'Graphic Designer',
+    Foto: 'barbara-ramos-graphic-designer.jpg'
+},
+]
 
 console.log(membriTeam)
 
